f00bf99ea4688735ec9815d5c6a9ab2aa4a13784
Marcus Vinicius Nascimento Ferreira <103126863+alucard93@users.noreply.github.com>
2022-12-26 14:12:20 -0300

    Revert "Feat/login"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 
 import './App.css';
-import Dashboard from './pages/Dashboard';
-// import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+    
     </div>
   );
 }

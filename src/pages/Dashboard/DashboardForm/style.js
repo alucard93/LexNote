26167f55d82564b44d56f9187b0b69ae73513739
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-12-26 15:49:36 -0300

    Update style.js

--- src/pages/Dashboard/DashboardForm/style.js
+++ src/pages/Dashboard/DashboardForm/style.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
 export const StyledDashboardForm = styled.div`
-    
+    padding: 1.5rem 1.5rem 2rem;
+    border: 1px solid rgba(255, 255, 255, 0.2);
+    border-radius: 12px;
 `;
